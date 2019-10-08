4242d2dd0fdaeffb705641b0931581fd55c270d9
Dimsun <Olivia>
2019-10-08 15:55:38 +0200

    Add some animation and assets

---
+++ flame_test/lib/langaw-game.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/gestures.dart';
+
+import 'backyard.dart';
+import 'fly.dart';
+
+class LangawGame extends Game {
+  Size screenSize;
+  double tileSize;
+  Backyard background;
+  List<Fly> flies;
+  Random rnd;
+
+  LangawGame() {
+    initialize();
+  }
+
+  void initialize() async {
+    flies = List<Fly>();
+    rnd = Random();
+    resize(await Flame.util.initialDimensions());
+    background = Backyard(this);
+    spawnFly();
+  }
+
+  void spawnFly() {
+    double x = rnd.nextDouble() * (screenSize.width - tileSize);
+    double y = rnd.nextDouble() * (screenSize.height - tileSize);
+    flies.add(Fly(this, x, y));
+  }
+
+  void render(Canvas canvas) {
+    background.render(canvas);
+    flies.forEach((Fly fly) => fly.render(canvas));
+  }
+
+  void update(double t) {
+    flies.forEach((Fly fly) => fly.update(t));
+    flies.removeWhere((Fly fly) => fly.isOffScreen);
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 9;
+  }
+
+  void onTapDown(TapDownDetails d) {
+    flies.forEach((Fly fly) {
+      if (fly.flyRect.contains(d.globalPosition)) {
+        fly.onTapDown();
+      }
+    });
+  }
+}
--- flame_test/lib/main.dart
+++ flame_test/lib/main.dart
@@ -1,18 +1,39 @@
-import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/util.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart';
-import 'box_game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'langaw-game.dart';
 
 void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
-  BoxGame game = BoxGame();
+  Flame.images.loadAll(<String>[
+    'bg/backyard.png',
+    'flies/agile-fly-1.png',
+    'flies/agile-fly-2.png',
+    'flies/agile-fly-dead.png',
+    'flies/drooler-fly-1.png',
+    'flies/drooler-fly-2.png',
+    'flies/drooler-fly-dead.png',
+    'flies/house-fly-1.png',
+    'flies/house-fly-2.png',
+    'flies/house-fly-dead.png',
+    'flies/hungry-fly-1.png',
+    'flies/hungry-fly-2.png',
+    'flies/hungry-fly-dead.png',
+    'flies/macho-fly-1.png',
+    'flies/macho-fly-2.png',
+    'flies/macho-fly-dead.png',
+  ]);
+
+  LangawGame game = LangawGame();
+  runApp(game.widget);
+
   TapGestureRecognizer tapper = TapGestureRecognizer();
   tapper.onTapDown = game.onTapDown;
-  runApp(game.widget);
   flameUtil.addGestureRecognizer(tapper);
 }
-
