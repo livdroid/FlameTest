ec8f4a34e6bdbdefa44e51cdbde327cd75dcdbe1
Dimsun <Olivia>
2019-10-08 15:04:50 +0200

    Basic Game

--- flame_test/lib/main.dart
+++ flame_test/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/gestures.dart';
+import 'box_game.dart';
 
-//void main() => runApp(MyGame().widget);
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  BoxGame game = BoxGame();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper);
+}
+
